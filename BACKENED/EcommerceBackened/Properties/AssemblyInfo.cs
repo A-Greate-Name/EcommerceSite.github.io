e6a0b7d57fdd4355034863d2780d204361a391d5
Kenneth Mokheth <Kennethmahlatsemokhethi@gmail.com>
2020-07-31 21:57:43 +0200

    Added new database

--- BACKENED/EcommerceBackened/Properties/AssemblyInfo.cs
+++ BACKENED/EcommerceBackened/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9b3957f7-a1d8-4573-b914-cc27cfcb1e5d")]
+[assembly: Guid("cf312bfd-4c96-49db-bce5-eaba7fd5f5eb")]
 
 // Version information for an assembly consists of the following four values:
 //
